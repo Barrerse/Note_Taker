707d05052a4f287f4e513756a40bae4921efc83e
Barrerse <sebastianbarrera@live.com>
2023-03-03 21:04:34 -0500

    Added server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const apiRoute = require('./routes/APIroutes');
+const htmlRoute = require('./routes/HTMLroutes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.use('/api', apiRoute);
+app.use('/', htmlRoute);
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+});
