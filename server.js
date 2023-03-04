200225f013d1f109105e87d8730c5e35c79827e7
Barrerse <sebastianbarrera@live.com>
2023-03-03 21:21:09 -0500

    commented and fixed server.js, added routes folder, added apiroutes

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const apiRoute = require('./routes/APIroutes');
-const htmlRoute = require('./routes/HTMLroutes');
-
-const PORT = process.env.PORT || 3001;
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true}));
-
-
-app.use(express.static('public'));
+// Set up middleware to handle JSON and urlencoded data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', apiRoute);
-app.use('/', htmlRoute);
+// Set up middleware to serve static assets from the public folder
+app.use(express.static('public'));
 
+// Set up routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Start server
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`);
 });
